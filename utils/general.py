7bf59f0ca59e75b40bfb16b83e3a243ef248584d
Rozhin Bayati <20394202+rozhin2025@users.noreply.github.com>
2024-03-28 21:30:52 +0100

    Update general.py

--- utils/general.py
+++ utils/general.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 safe_cast_to_f16 = lambda x: np.round(x, 3).astype(np.float16)
 
@@ -12,6 +13,24 @@ def generate_subsets(set):
     if set == []: return [[]]
     subsets = generate_subsets(set[1:])
     return subsets + [[set[0]] + subset for subset in subsets]
+
+
+def convert_classification_report_to_df(report_dict):
+    '''
+    Converts the classification report dictionary to a pandas DataFrame
+    :param report_dict: classification report dictionary
+    :return: pandas DataFrame
+    '''
+    record = {}
+    for k, v in report_dict.items():
+        if isinstance(v, dict):
+            for k_, v_ in v.items():
+                record[(k, k_)] = v_
+        else:
+            record[('', k)] = v
+    record = pd.DataFrame.from_records(record, index=[0])
+    record.columns = pd.MultiIndex.from_tuples(record.columns)
+    return record
 
 
 if __name__ == "__main__":
